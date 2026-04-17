4d515a6d7068c178f2bb37653262cc393b09fb35
matehus064 <matheus.drosa@sptech.school>
2026-04-17 00:21:16 -0300

    Ajuste completo CSS, integracao de paginas, paginas de login e cadastro

---
+++ AliancaApis - Site/script.js
@@ -0,0 +1,43 @@
+function entrar() {
+    let email = ipt_email.value;
+    let senha = ipt_senha.value;
+    if (email == "usuario@aliacaapis.com" && senha == "urubu100") {
+        window.location.href = "dashboardPH.html";
+    } else {
+        alert("Informações incorretas!");
+    }
+}
+
+function cadastrar() {
+    let nome = ipt_nome.value;
+    let cpf = ipt_cpf.value;
+    let email = ipt_email.value;
+    let senha = ipt_senha.value;
+    let confirmacaoSenha = ipt_confirmacaoSenha.value;
+
+    let condicoesErro = [
+        nome == "" || nome.length <= 1,
+        cpf.length !== 11,
+        !email.includes("@"),
+        senha.length < 6,
+        confirmacaoSenha !== senha,
+    ];
+
+    let mensagensErro = [
+        "Nome inválido!",
+        "CPF deve ter 11 dígitos!",
+        "E-mail deve conter @",
+        "Senha muito curta (mínimo 6)!",
+        "Senhas não coincidem!"
+    ];
+
+    for (let i = 0; i < condicoesErro.length; i++) {
+
+        if (condicoesErro[i]) {
+            div_erro.innerHTML = mensagensErro[i];
+
+            return false;
+        }
+    }
+    alert('INDISPONÍVEL NO MOMENTO, TENTE NOVAMENTE MAIS TARDE')
+}
