e9103f1b9b1f71dac3b4b120a66399840015e728
matehus064 <matheus.drosa@sptech.school>
2026-04-19 01:41:54 -0300

    fix: css - página de login e cadastro com css unificado, adição de conteúdo a página inicial

--- AliancaApis - Site/script.js
+++ AliancaApis - Site/script.js
@@ -2,10 +2,11 @@ function entrar() {
     let email = ipt_email.value;
     let senha = ipt_senha.value;
     if (email == "usuario@aliacaapis.com" && senha == "urubu100") {
-        window.location.href = "dashboardPH.html";
+        window.location.href = "dashboard.html";
     } else {
         alert("Informações incorretas!");
     }
+    console.log(senha)
 }
 
 function cadastrar() {
@@ -42,4 +43,47 @@ function cadastrar() {
         }
     }
     alert('INDISPONÍVEL NO MOMENTO, TENTE NOVAMENTE MAIS TARDE')
+}
+
+// CALCULADORA / SIMULADOR FINANCEIRO
+    let enxamePreco = 450;
+    let custoSensor = 75;
+    let custoCaixas = 200;
+
+    function calculo(){
+        let preco = Number(ipt_preco.value);
+        let kilos = Number(ipt_kg.value);
+        let qtdColmeia = Number(ipt_quantidade.value);
+        let valorMan = Number(ipt_manutencao.value)
+        let venda = preco * kilos * qtdColmeia;
+        let custoManutencao = valorMan * qtdColmeia;
+        let lucro = (venda * 0.30) - custoManutencao;
+        let reposicao = ((qtdColmeia * 0.30) * custoCaixas) + ((qtdColmeia * 0.30) * enxamePreco);
+        let custoTotal = custoSensor * qtdColmeia;
+        let roi = ((lucro - custoTotal) / custoTotal) * 100
+        let contrato = contrata.value
+
+        divMsg.innerHTML = " ";
+
+        if(contrato == 's'){
+
+        divMsg.innerHTML += `Obrigado por confiar em nós da Aliança Apis<br>`
+
+        divMsg.innerHTML += `Com o nosso serviço contratado você está evitando uma perda de até R$${(lucro + reposicao).toFixed(2)}.<br>Contando com a receita da produção de mel em 100% e com reposição de colmeias<br>`
+
+        divMsg.innerHTML += `Assim obtendo um ROI de ${roi.toFixed(0)}%`
+
+    } else {
+
+        if(kilos < 45){
+            divMsg.innerHTML += `<span style="color: red">Suas abelhas produzem abaixo do nível esperado pode haver algo de errado!!</span><br>`
+        }
+
+        divMsg.innerHTML += `Caso você não adquira nosso serviço você pode deixar de ter um faturamento de R$${lucro.toFixed(2)}, considerando que 36% da população geral de abelhas é afetada pelo DCC!<br>`
+
+        divMsg.innerHTML += `Além do faturamento perdido você terá um prejuizo de R$${reposicao.toFixed(2)} de reposição.<br> Com os gastos de novos enxames e aquisição de novas caixas colmeia.<br>`
+
+        divMsg.innerHTML += `Caso adquira o nosso serviço que custaria R$${custoTotal.toFixed(2)} você terá um ROI estimado de ${roi.toFixed(0)}%`
+    }
+    
 }
