5850de9ff7273a844929d9dd436ca4af5ea491e7
mariaClara-AZVD <maria.azevedo@sptech.school>
2026-06-02 12:26:03 -0300

    Validação de cargo

--- AliancaApis - Site/src/controllers/usuarioController.js
+++ AliancaApis - Site/src/controllers/usuarioController.js
@@ -4,6 +4,7 @@ var aquarioModel = require("../models/aquarioModel");
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var classe = req.body.classeServer;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -27,6 +28,7 @@ function autenticar(req, res) {
                                     email: resultadoAutenticar[0].email,
                                     nome: resultadoAutenticar[0].nome,
                                     senha: resultadoAutenticar[0].senha,
+                                    classe: resultadoAutenticar[0].classe,
                                     empresaId: resultadoAutenticar[0].empresaId,
                                     aquarios: resultadoAquarios
                                 });
@@ -86,6 +88,45 @@ function cadastrar(req, res) {
             );
     }
 }
+
+// function cadastrar(req, res) {
+   
+//     var nome = req.body.nomeServer;
+//     var email = req.body.emailServer;
+//     var senha = req.body.senhaServer;
+
+//     fkEmpresa = Number(fkEmpresa);
+
+//     // Faça as validações dos valores
+//     if (nome == undefined) {
+//         res.status(400).send("Seu nome está undefined!");
+//     } else if (email == undefined) {
+//         res.status(400).send("Seu email está undefined!");
+//     } else if (senha == undefined) {
+//         res.status(400).send("Sua senha está undefined!");
+//     } else if (fkEmpresa == undefined) {
+//         res.status(400).send("Sua empresa a vincular está undefined!");
+//     } else {
+
+//         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+//         usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+//             .then(
+//                 function (resultado) {
+//                     res.json(resultado);
+//                 }
+//             ).catch(
+//                 function (erro) {
+//                     console.log(erro);
+//                     console.log(
+//                         "\nHouve um erro ao realizar o cadastro! Erro: ",
+//                         erro.sqlMessage
+//                     );
+//                     res.status(500).json(erro.sqlMessage);
+//                 }
+//             );
+//     }
+// }
+
 
 module.exports = {
     autenticar,
