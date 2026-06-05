3c4cb6c057a7d2a25c8a15543a5fe314d8a8ecc1
matehus064 <matheus.drosa@sptech.school>
2026-06-05 17:05:04 -0300

    add: verificação de usuario e ajuste css

---
+++ public/js/gestor.js
@@ -0,0 +1,8 @@
+const classeUsuario = sessionStorage.getItem("CLASSE");
+
+if (classeUsuario == "gestor") {
+    sidebar.innerHTML += `
+        <a href="../cadastro.html"><i class="fa-solid fa-user-plus"></i>Cadastro</a>
+        <a href="https://aliancaapis.atlassian.net/servicedesk/customer/portal/1"><i class="fa-solid fa-headset"></i>Central de Atendimento</a>
+    `
+}
