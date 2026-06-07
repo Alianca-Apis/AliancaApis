ae2bd51d3fd231258c7566dbec3aadfe02943ed4
matehus064 <matheus.drosa@sptech.school>
2026-06-07 03:25:32 -0300

    fix: ajustes de arquivos e css

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,4 +1,3 @@
-// sessão
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
@@ -17,20 +16,17 @@ function limparSessao() {
     window.location = "../login.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+const classeUsuario = sessionStorage.getItem("CLASSE");
+
+if (classeUsuario == "gestor") {
+    sidebar.innerHTML += `
+        <a href="../cadastro.html"><i class="fa-solid fa-user-plus"></i>Cadastro</a>
+        <a href="https://aliancaapis.atlassian.net/servicedesk/customer/portal/1"><i class="fa-solid fa-headset"></i>Central de Atendimento</a>
+        <hr>   
+        <a onclick="limparSessao()" href="" id="btn-sair"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+        `
+} else {
+    sidebar.innerHTML += `
+        <a onclick="limparSessao()" href="" id="btn-sair"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+        `
 }
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
